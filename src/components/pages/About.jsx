9b0df27d7b57448b13a39a4e2b303b8efdb01f2c
hedaetul <headaetulislam143@gmail.com>
2024-01-05 00:55:39 +0600

    create buttons-inc and display-count components.modified about.jsx file

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,22 +2,43 @@ import { useState } from "react";
 import ButtonInc from "../buttons-inc/ButtonInc";
 import DisplayCount from "../display-count/DisplayCount";
 import Layout from "../layout/Layout";
+import UpdateIncrementDecrement from "../update-inc/UpdateIncrementDecrement";
 
 function About() {
   const [count, setCount] = useState(100);
+  const [incrementValue, setIncrementValue] = useState(10);
+  const [decrementValue, setDecrementValue] = useState(10);
+
+  const o = count || 50
+  console.log('value of 0', o);
+
+  count && console.log('count has a value', o);
 
   function increment() {
-    setCount(count + 1);
+    setCount(count + incrementValue);
   }
 
   function decrement() {
-    setCount(count - 1);
+    setCount(count - decrementValue);
+  }
+
+  function handleIncrementChange(e) {
+    setIncrementValue(parseInt(e.target.value));
+  }
+  function handleDecrementChange(e) {
+    setDecrementValue(parseInt(e.target.value));
   }
 
   return (
     <Layout>
       <h1>I am About page</h1>
       <DisplayCount count={count} />
+      <UpdateIncrementDecrement
+        incrementValue={incrementValue}
+        decrementValue={decrementValue}
+        handleIncrementChange={handleIncrementChange}
+        handleDecrementChange={handleDecrementChange}
+      />
       <ButtonInc increment={increment} decrement={decrement} />
     </Layout>
   );
--- src/components/pages/Help.jsx
+++ src/components/pages/Help.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import Layout from "../layout/Layout";
 
 function Help() {
+  // const [state, setState] = useState({});
+  const [name, setName] = useState("Hedaetul");
+
+  // setTimeout(() => {
+  //   setState({ name: "Hedaetul" });
+  // }, 1000);
+  // console.log("Rendering");
+
   return (
     <Layout>
-      <h1>Hello, I am Help page</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus
-        eos quos, corrupti hic omnis alias quidem, reiciendis labore sit placeat
-        natus nihil vitae eaque! Ipsum obcaecati voluptate sit consequatur 
-        dolorem.
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit et ex
-        labore omnis quae nulla aliquid laborum dolor deserunt asperiores, iusto
-        modi amet facilis. Dolore debitis temporibus ut ipsa voluptatum.
-      </p>
+      {/*name && <h1>Hello {name}, I am Help age</h1>}
+  {!name && <h1>Hello Guist, I am Help page</h1>*/}
+
+      {name ? (
+        <h1>Hello {name}, I am Help age</h1>
+      ) : (
+        <h1>Hello Guist, I am Help page</h1>
+      )}
     </Layout>
   );
 }
